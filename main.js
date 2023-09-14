a3fab59ce81d49741491f603e15859e8d5a16e24
Julia Mayumi <julia.mayumi.costa@escola.pr.gov.br>
2023-09-14 08:14:10 -0300

    asasas

--- main.js
+++ main.js
@@ -5,5 +5,9 @@ const lista = document.querySelectorAll('.tecla');
 
 let Contador = 0;
 
+while (Contador < 9) {
+    lista[0].onclick = Toca;
+    Contador = Contador + 1;
+}
 
-lista[0].onclick = Toca;
+
