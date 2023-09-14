06b6bf7548c1e4c1eca64a53c7421c1c98a7b415
Julia Mayumi <julia.mayumi.costa@escola.pr.gov.br>
2023-09-14 08:02:44 -0300

    aaaaa

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function Toca () {
     document.querySelector('#som_tecla_pom').play();
 }
+const lista = document.querySelectorAll('.tecla');
 
 document.querySelector('.tecla_pom').onclick = Toca;
