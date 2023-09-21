80c05b7d89e61ba995dc6c96f451258de6d06cf3
Julia Mayumi <julia.mayumi.costa@escola.pr.gov.br>
2023-09-21 08:22:05 -0300

    c

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-function Toca () {
-    document.querySelector('#som_tecla_pom').play();
+function Toca (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 const lista = document.querySelectorAll('.tecla');
 
 let Contador = 0;
 
-while (Contador < 9) {
-    lista[Contador].onclick = Toca;
+while (Contador < lista.length) {
+        lista[Contador].onclick = function () {
+            Toca ('#som_tecla_toim');
+        }
     const classe = lista[Contador].classList[1];
     console.log(classe);
     Contador = Contador + 1;
