825fdceedd70dfd4fec6b2e4d2ae8bfd660b3334
ApolloBRibeiro <131731148+ApolloBRibeiro@users.noreply.github.com>
2023-09-29 09:39:12 -0300

    bx

--- main.js
+++ main.js
@@ -6,15 +6,20 @@ const lista = document.querySelectorAll('.tecla');
 let Contador = 0;
 
 while (Contador < lista.length) {
-    lista[Contador].onclick = function () {
-        Toca('#som_tecla_toim');
-    }
+
     const classe = lista[Contador].classList[1];
     console.log(classe);
-
+    
     const idAudio = `#som_${classe}`;
     console.log(idAudio);
+
+    lista[Contador].onclick = function () {
+        Toca(idAudio);
+    }
+   
+
     
+
     Contador = Contador + 1;
     console.log(Contador);
 
